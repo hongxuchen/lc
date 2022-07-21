9367b45bea0bf01a4a89789b39d90b481dbe049f
c00593292 <chenhongxu5@huawei.com>
2022-07-21 23:20:56 +0800

    fix 0705

--- src/main/java/p/s0705/MyHashSet.java
+++ src/main/java/p/s0705/MyHashSet.java
@@ -1,14 +1,18 @@
 package p.s0705;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyHashSet {
 
     int SIZE = 1000001;
-    List<Integer>[] buckets = new LinkedList[SIZE];
+    List<List<Integer>> buckets;
 
     public MyHashSet() {
+        buckets = new ArrayList<>(SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            buckets.add(new ArrayList<>());
+        }
     }
 
     private int getBucket(int key) {
@@ -21,33 +25,22 @@ public class MyHashSet {
 
     public void add(int key) {
         int i = getBucket(key);
-        List<Integer> list = buckets[i];
-        if (list == null) {
-            list = new LinkedList<>();
+        List<Integer> list = buckets.get(i);
+        boolean found = list.contains(key);
+        if (!found) {
             list.add(key);
-        } else {
-            boolean found = list.contains(key);
-            if (!found) {
-                list.add(key);
-            }
         }
     }
 
     public void remove(int key) {
         int i = getBucket(key);
-        List<Integer> list = buckets[i];
-        if (list == null) {
-            return;
-        }
+        List<Integer> list = buckets.get(i);
         list.removeIf(ele -> ele == key);
     }
 
     public boolean contains(int key) {
         int i = getBucket(key);
-        List<Integer> list = buckets[i];
-        if (list == null) {
-            return false;
-        }
+        List<Integer> list = buckets.get(i);
         return list.contains(key);
     }
 
